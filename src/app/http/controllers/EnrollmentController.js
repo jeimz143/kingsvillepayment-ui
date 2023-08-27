f426b20a6ef3cc68dc8fa374b112d4245b55194a
Jeimson Medina <jeimzm@gmail.com>
2023-08-27 19:51:55 +0800

    refactor code for download SOA

--- src/app/http/controllers/EnrollmentController.js
+++ src/app/http/controllers/EnrollmentController.js
@@ -359,25 +359,30 @@ module.exports = {
         var tuitionFee = enrollee.fees.find((rf) => rf.name === 'Tuition Fees')
         var miscellanousFee = enrollee.fees.find((rf) => rf.name === 'Miscellaneous Fees')
         var eventFee = enrollee.fees.find((rf) => rf.name === 'Event Fee')
-        var parangalFee = enrollee.fees.find((rf) => rf.name === 'Parangal Fee')
+        // var parangalFee = enrollee.fees.find((rf) => rf.name === 'Parangal Fee')
         var fieldTrip = enrollee.fees.find((rf) => rf.name === 'Field Trip')
         var royalBall = enrollee.fees.find((rf) => rf.name === 'Royal Ball')
         var yearBook = enrollee.fees.find((rf) => rf.name === 'Annual Yearbook Graduating')
         var framedGradPicture = enrollee.fees.find((rf) => rf.name === 'Framed Grad Piture')
         var diploma = enrollee.fees.find((rf) => rf.name === 'Framed Diploma, Theca, Framed Grad Picture')
         var supplies = enrollee.fees.find((rf) => rf.name === 'Supplies - All Students')
-        var pins = enrollee.fees.filter((rf) => rf.name === 'Pin')
-        var nameplates = enrollee.fees.filter((rf) => rf.name === 'Nameplate')
-        var uniforms = enrollee.fees.filter((rf) => rf.name === 'Uniform')
-        var vans = enrollee.fees.filter((rf) => rf.name === 'Van')
-    
+        // var pins = enrollee.fees.filter((rf) => rf.name === 'Pin')
+        // var nameplates = enrollee.fees.filter((rf) => rf.name === 'Nameplate')
+        // var uniforms = enrollee.fees.filter((rf) => rf.name === 'Uniform')
+        // var vans = enrollee.fees.filter((rf) => rf.name === 'Van')
+
+        /*
+        *
+        *
+        * Required Fees section
+        * */
         if (!_.isEmpty(books)) {
           soalist.push([
             (books.payments[0].datePaid && books.payments[0].isPaid) ? moment(books.payments[0].datePaid).format('MM/DD/YYYY') : '',
             (books.payments[0].userId && books.payments[0].isPaid) ? `${books.payments[0].userId.givenName} ${books.payments[0].userId.lastName}` : '',
             (books.payments[0].receipt && books.payments[0].isPaid) ? books.payments[0].receipt.orNumber : 'None',
             books.payments[0].amountToPayPerMonth,
-            '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+            '', '', '', '', '', '', '', '', '', '', '', '', '', '',
             books.payments[0].amountDue
           ])
         }
@@ -387,7 +392,7 @@ module.exports = {
             (registrationFee.payments[0].userId) ? `${registrationFee.payments[0].userId.givenName} ${registrationFee.payments[0].userId.lastName}` : '',
             (registrationFee.payments[0].receipt) ? registrationFee.payments[0].receipt.orNumber : '',
             '',
-            registrationFee.payments[0].amountToPayPerMonth, '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+            registrationFee.payments[0].amountToPayPerMonth, '', '', '', '', '', '', '', '', '', '', '', '', '',
             registrationFee.payments[0].amountDue
           ])
         }
@@ -447,6 +452,181 @@ module.exports = {
           })
           rowNumber++
         })
+
+        /*
+        *
+        *
+        * Anticipated Events section
+        * */
+
+        if (eventFee && eventFee.payments.length !== 0) {
+          if (eventFee.payments[0].isPaid) {
+            workSheet.getCell('A23').value = moment(eventFee.payments[0].datePaid).format('MM/DD/YYYY')
+            workSheet.getCell('B23').value = (eventFee.payments[0].userId) ? `${eventFee.payments[0].userId.givenName} ${eventFee.payments[0].userId.lastName}` : ''
+            workSheet.getCell('C23').value = eventFee.payments[0].receipt.orNumber
+            workSheet.getCell('H23').value = (eventFee.payments[0].amountDue) ? eventFee.payments[0].amountDue : ''
+            workSheet.getCell('I23').value = (eventFee.payments[0].isPaid) ? 'Paid' : 'Unpaid'
+          }
+        }
+
+        if (fieldTrip && fieldTrip.payments.length !== 0) {
+          if (fieldTrip.payments[0].isPaid) {
+            workSheet.getCell('A24').value = moment(fieldTrip.payments[0].datePaid).format('MM/DD/YYYY')
+            workSheet.getCell('B24').value = (fieldTrip.payments[0].userId) ? `${fieldTrip.payments[0].userId.givenName} ${fieldTrip.payments[0].userId.lastName}` : ''
+            workSheet.getCell('C24').value = fieldTrip.payments[0].receipt.orNumber
+            workSheet.getCell('H24').value = (fieldTrip.payments[0].amountDue) ? fieldTrip.payments[0].amountDue : ''
+            workSheet.getCell('I24').value = (fieldTrip.payments[0].isPaid) ? 'Paid' : 'Unpaid'
+          }
+        }
+
+        if (royalBall && royalBall.payments.length !== 0) {
+          if (royalBall.payments[0].isPaid) {
+            workSheet.getCell('A25').value = moment(royalBall.payments[0].datePaid).format('MM/DD/YYYY')
+            workSheet.getCell('B25').value = (royalBall.payments[0].userId) ? `${royalBall.payments[0].userId.givenName} ${royalBall.payments[0].userId.lastName}` : ''
+            workSheet.getCell('C25').value = royalBall.payments[0].receipt.orNumber
+            workSheet.getCell('H25').value = (royalBall.payments[0].amountDue) ? royalBall.payments[0].amountDue : ''
+            workSheet.getCell('I25').value = (royalBall.payments[0].isPaid) ? 'Paid' : 'Unpaid'
+          }
+        }
+
+        if (supplies && supplies.payments.length !== 0) {
+          if (supplies.payments[0].isPaid) {
+            workSheet.getCell('A26').value = moment(supplies.payments[0].datePaid).format('MM/DD/YYYY')
+            workSheet.getCell('B26').value = (supplies.payments[0].userId) ? `${supplies.payments[0].userId.givenName} ${supplies.payments[0].userId.lastName}` : ''
+            workSheet.getCell('C26').value = supplies.payments[0].receipt.orNumber
+            workSheet.getCell('H26').value = (supplies.payments[0].amountDue) ? supplies.payments[0].amountDue : ''
+            workSheet.getCell('I26').value = (supplies.payments[0].isPaid) ? 'Paid' : 'Unpaid'
+          }
+        }
+
+        var coat = enrollee.fees.filter((rf) => rf.name.includes('Coat') || rf.name.includes('Textile'))
+        var coatTotalAmount = 0
+        var coatReceipts = []
+        var coatUser = []
+        var unpaidCoat = 0
+        var paidCoat = 0
+        coat.forEach((coatItem, coatIndex) => {
+          if (coatItem.payments.length !== 0) {
+            if (coatItem.payments[0].isPaid) {
+              coatTotalAmount += coatItem.payments[0].amountDue
+              coatReceipts.push(coatItem.payments[0].receipt.orNumber)
+              coatUser.push((coatItem.payments[0].userId) ? `${coatItem.payments[0].userId.givenName} ${coatItem.payments[0].userId.lastName}` : '')
+              paidCoat += 1
+            } else {
+              unpaidCoat += 1
+            }
+          }
+        })
+
+        if (coatTotalAmount !== 0) {
+          workSheet.getCell('A27').value = ''
+          workSheet.getCell('B27').value = coatUser
+          workSheet.getCell('C27').value = coatReceipts
+          workSheet.getCell('H27').value = coatTotalAmount
+          workSheet.getCell('I27').value = `Paid: ${paidCoat}, Unpaid: ${unpaidCoat}`
+        }
+
+        var uniform = enrollee.fees.filter((rf) => rf.name.includes('PE') || rf.name.includes('KVS'))
+        var uniformTotalAmount = 0
+        var uniformReceipts = []
+        var uniformUser = []
+        var unpaiduniform = 0
+        var paiduniform = 0
+        uniform.forEach((uniformItem, uniformIndex) => {
+          if (uniformItem.payments.length !== 0) {
+            if (uniformItem.payments[0].isPaid) {
+              uniformTotalAmount += uniformItem.payments[0].amountDue
+              uniformReceipts.push(uniformItem.payments[0].receipt.orNumber)
+              uniformUser.push((uniformItem.payments[0].userId) ? `${uniformItem.payments[0].userId.givenName} ${uniformItem.payments[0].userId.lastName}` : '')
+              paiduniform += 1
+            } else {
+              unpaiduniform += 1
+            }
+          }
+        })
+        if (uniformTotalAmount !== 0) {
+          workSheet.getCell('A28').value = ''
+          workSheet.getCell('B28').value = uniformUser
+          workSheet.getCell('C28').value = uniformReceipts
+          workSheet.getCell('H28').value = uniformTotalAmount
+          workSheet.getCell('I28').value = `Paid: ${paiduniform}, Unpaid: ${unpaiduniform}`
+        }
+
+        var pinNameplate = enrollee.fees.filter((rf) => rf.name.includes('Nameplate') || rf.name.includes('Pin'))
+        var pinNameplateTotalAmount = 0
+        var pinNameplateReceipts = []
+        var pinNameplateUser = []
+        var unpaidpinNameplate = 0
+        var paidpinNameplate = 0
+        pinNameplate.forEach((pinNameplateItem, pinNameplateIndex) => {
+          if (pinNameplateItem.payments.length !== 0) {
+            if (pinNameplateItem.payments[0].isPaid) {
+              pinNameplateTotalAmount += pinNameplateItem.payments[0].amountDue
+              pinNameplateReceipts.push(pinNameplateItem.payments[0].receipt.orNumber)
+              pinNameplateUser.push((pinNameplateItem.payments[0].userId) ? `${pinNameplateItem.payments[0].userId.givenName} ${pinNameplateItem.payments[0].userId.lastName}` : '')
+              paidpinNameplate += 1
+            } else {
+              unpaidpinNameplate += 1
+            }
+          }
+        })
+        if (pinNameplateTotalAmount !== 0) {
+          workSheet.getCell('A29').value = ''
+          workSheet.getCell('B29').value = pinNameplateUser
+          workSheet.getCell('C29').value = pinNameplateReceipts
+          workSheet.getCell('H29').value = pinNameplateTotalAmount
+          workSheet.getCell('I29').value = `Paid: ${paidpinNameplate}, Unpaid: ${unpaidpinNameplate}`
+        }
+
+        /*
+        *
+        *
+        * Selective Fees
+        * */
+
+        var pfCellNumber = (tlevelCode === '01' || tlevelCode === '06' || tlevelCode === '12') ? 23 : 24
+        var parangalFee = enrollee.fees.find((rf) => rf.name === 'Parangal Fee')
+        if (parangalFee && parangalFee.payments.length !== 0) {
+          if (parangalFee.payments[0].isPaid) {
+            workSheet.getCell(`J${pfCellNumber}`).value = moment(parangalFee.payments[0].datePaid).format('MM/DD/YYYY')
+            workSheet.getCell(`K${pfCellNumber}`).value = (parangalFee.payments[0].userId) ? `${parangalFee.payments[0].userId.givenName} ${parangalFee.payments[0].userId.lastName}` : ''
+            workSheet.getCell(`L${pfCellNumber}`).value = parangalFee.payments[0].receipt.orNumber
+            workSheet.getCell(`R${pfCellNumber}`).value = parangalFee.payments[0].amountDue
+            workSheet.getCell(`S${pfCellNumber}`).value = (parangalFee.payments[0].isPaid) ? 'Paid' : 'Unpaid'
+          }
+        }
+
+        if (yearBook && yearBook.payments.length !== 0) {
+          if (yearBook.payments[0].isPaid) {
+            workSheet.getCell('J25').value = moment(yearBook.payments[0].datePaid).format('MM/DD/YYYY')
+            workSheet.getCell('K25').value = (yearBook.payments[0].userId) ? `${yearBook.payments[0].userId.givenName} ${yearBook.payments[0].userId.lastName}` : ''
+            workSheet.getCell('L25').value = yearBook.payments[0].receipt.orNumber
+            workSheet.getCell('R25').value = parangalFee.payments[0].amountDue
+            workSheet.getCell('S25').value = (yearBook.payments[0].isPaid) ? 'Paid' : 'Unpaid'
+          }
+        }
+
+        var frameGradPic = enrollee.fees.find((rf) => rf.name === 'Framed Grad Piture')
+        if (frameGradPic && frameGradPic.payments.length !== 0) {
+          if (frameGradPic.payments[0].isPaid) {
+            workSheet.getCell('J26').value = moment(frameGradPic.payments[0].datePaid).format('MM/DD/YYYY')
+            workSheet.getCell('K26').value = (frameGradPic.payments[0].userId) ? `${frameGradPic.payments[0].userId.givenName} ${frameGradPic.payments[0].userId.lastName}` : ''
+            workSheet.getCell('L26').value = frameGradPic.payments[0].receipt.orNumber
+            workSheet.getCell('R26').value = frameGradPic.payments[0].amountDue
+            workSheet.getCell('S26').value = (frameGradPic.payments[0].isPaid) ? 'Paid' : 'Unpaid'
+          }
+        }
+
+        var frameGradPicTheca = diploma
+        if (frameGradPicTheca && frameGradPicTheca.payments.length !== 0) {
+          if (frameGradPicTheca.payments[0].isPaid) {
+            workSheet.getCell('J27').value = moment(frameGradPicTheca.payments[0].datePaid).format('MM/DD/YYYY')
+            workSheet.getCell('K27').value = (frameGradPicTheca.payments[0].userId) ? `${frameGradPicTheca.payments[0].userId.givenName} ${frameGradPicTheca.payments[0].userId.lastName}` : ''
+            workSheet.getCell('L27').value = frameGradPicTheca.payments[0].receipt.orNumber
+            workSheet.getCell('R27').value = frameGradPic.payments[0].amountDue
+            workSheet.getCell('S27').value = (frameGradPicTheca.payments[0].isPaid) ? 'Paid' : 'Unpaid'
+          }
+        }
 
         workbook.xlsx.writeFile('storage/downloads/StatementOfAccount.xlsx').then(function () {
           res.download('storage/downloads/StatementOfAccount.xlsx', function (err) {
